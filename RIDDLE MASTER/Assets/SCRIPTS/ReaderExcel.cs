3253e74e7c25839b0474f4851511abcaae537ad4
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-06-04 13:51:20 -0300

    i added ten more questions

--- RIDDLE MASTER/Assets/SCRIPTS/ReaderExcel.cs
+++ RIDDLE MASTER/Assets/SCRIPTS/ReaderExcel.cs
@@ -23,7 +23,7 @@ public class ReaderExcel : MonoBehaviour
     public void readingExcel(List<QuestionSO> arrayExcel)
     {
         string[] data = excel.text.Split(new string[] { ",", "\n" }, StringSplitOptions.None);
-        for (int i = 0; i < 45; i++)
+        for (int i = 0; i < 55; i++)
         {
             arrayExcel[i].setQuestion(data[6 * (i + 1)]);
             // Debug.Log(data[3 * (i + 1)]);
